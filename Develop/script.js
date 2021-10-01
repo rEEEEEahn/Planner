0eb31972d9d54a9f1f5f1f66a95be11485a8a9e0
Ryan Hsiao <rdjr.hsiao@gmail.com>
2021-09-30 18:45:22 -0700

    fixed save button still need read me

--- Develop/script.js
+++ Develop/script.js
@@ -33,14 +33,24 @@ time.each(function () {
 }
 currentTime()
 
-
 button.on('click', function (event) {
   event.preventDefault();
-  var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
+  var text = $(this).siblings('.col-md-10').val().replace(/['"]+/g, '');
   var parent = $(this).parent().attr('id');
   localStorage.setItem(parent, JSON.stringify(text));
 
-  })
+})
+$('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
+$('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
+$('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
+$('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
+$('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
+$('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
+$('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
+$('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
+$('#17 textarea').val(localStorage.getItem('17')).replace(/['"]+/g, '');
+
+
  // function to update row color
 
 //saves
